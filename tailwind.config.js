b8a864db3cae57fe0d9bf9c7de424aafd075d982
SiThuKyaw-STK <sithukyaw.stk.89@gmail.com>
2023-03-16 13:13:11 +0630

    Nessessory setup finished

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors:{
+      'primary': '#00ADB5',
+      'secondary': '#393E46',
+      'info': '#0097FA',
+      'white': '#EEEEEE',
+    }
+  },
+  plugins: [],
+}
