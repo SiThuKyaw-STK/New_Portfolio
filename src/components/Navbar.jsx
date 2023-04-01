12785a9c1c32c6891f92094c638a415baa258d97
SiThuKyaw-STK <sithukyaw.stk.89@gmail.com>
2023-04-01 18:52:54 +0630

    mobile navbar setup finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {IoHome} from "react-icons/io5";
 import {BsFillInfoCircleFill, BsFillPersonVcardFill} from "react-icons/bs";
 import {FaFolderOpen, FaUserGraduate} from "react-icons/fa";
 import Link from "next/link";
+import {CgArrowsExchange} from "react-icons/cg";
 
 const Navbar = () => {
+    const [nav,setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav);
+    }
     return (
         <>
-            <nav className={`z-[1000] md:grid hidden w-[50px] hover:w-[150px] overflow-hidden grid grid-rows-5 gap-5 bg-secondary rounded-xl fixed top-[30%] right-[1%] duration-700 ease-in-out shadow-inner-nav`}>
-                <Link href={`/`} className={`group flex items-center justify-start text-primary p-3 cursor-pointer hover:bg-primary duration-500 ease-in-out`}>
-                    <IoHome className={`text-2xl group-hover:text-secondary duration-500 ease-in-out`}/>
-                    <span className={`font-medium uppercase ml-4 group-hover:text-secondary duration-500 ease-in-out`}>Home</span>
+            <nav className={`z-[1000] w-[50px] hover:w-[150px] overflow-hidden hidden md:grid grid-rows-5 gap-5 bg-secondary rounded-xl fixed top-[30%] right-[1%] duration-700 ease-in-out`}>
+                <Link href={`/`} className={`group flex items-center justify-start text-primary p-3 cursor-pointer hover:bg-primary animation-500`}>
+                    <IoHome className={`text-2xl group-hover:text-secondary animation-500`}/>
+                    <span className={`font-medium uppercase ml-4 group-hover:text-secondary`}>Home</span>
                 </Link>
-                <Link href={`/about`} className={`group flex items-center justify-start text-primary p-3 cursor-pointer hover:bg-primary duration-500 ease-in-out`}>
-                    <BsFillInfoCircleFill className={`text-2xl group-hover:text-secondary duration-500 ease-in-out`}/>
-                    <span className={`font-medium uppercase ml-4 group-hover:text-secondary duration-500 ease-in-out`}>About</span>
+                <Link href={`/about`} className={`group flex items-center justify-start text-primary p-3 cursor-pointer hover:bg-primary animation-500`}>
+                    <BsFillInfoCircleFill className={`text-2xl group-hover:text-secondary animation-500`}/>
+                    <span className={`font-medium uppercase ml-4 group-hover:text-secondary animation-500`}>About</span>
                 </Link>
-                <Link href={`/education`} className={`group flex items-center justify-start text-primary p-3 cursor-pointer hover:bg-primary duration-500 ease-in-out`}>
-                    <FaUserGraduate className={`text-2xl group-hover:text-secondary duration-500 ease-in-out`}/>
-                    <span className={`font-medium uppercase ml-4 group-hover:text-secondary duration-500 ease-in-out`}>Education</span>
+                <Link href={`/education`} className={`group flex items-center justify-start text-primary p-3 cursor-pointer hover:bg-primary animation-500`}>
+                    <FaUserGraduate className={`text-2xl group-hover:text-secondary animation-500`}/>
+                    <span className={`font-medium uppercase ml-4 group-hover:text-secondary animation-500`}>Education</span>
                 </Link>
-                <Link href={`/projects`} className={`group flex items-center justify-start text-primary p-3 cursor-pointer hover:bg-primary duration-500 ease-in-out`}>
-                    <FaFolderOpen className={`text-2xl group-hover:text-secondary duration-500 ease-in-out`}/>
-                    <span className={`font-medium uppercase ml-4 group-hover:text-secondary duration-500 ease-in-out`}>Projects</span>
+                <Link href={`/projects`} className={`group flex items-center justify-start text-primary p-3 cursor-pointer hover:bg-primary animation-500`}>
+                    <FaFolderOpen className={`text-2xl group-hover:text-secondary animation-500`}/>
+                    <span className={`font-medium uppercase ml-4 group-hover:text-secondary animation-500`}>Projects</span>
                 </Link>
-                <Link href={`/cv`} className={`group flex items-center justify-start text-primary p-3 cursor-pointer hover:bg-primary duration-500 ease-in-out`}>
-                    <BsFillPersonVcardFill className={`text-2xl group-hover:text-secondary duration-500 ease-in-out`}/>
-                    <span className={`font-medium uppercase ml-4 group-hover:text-secondary duration-500 ease-in-out`}>CV Form</span>
+                <Link href={`/cv`} className={`group flex items-center justify-start text-primary p-3 cursor-pointer hover:bg-primary animation-500`}>
+                    <BsFillPersonVcardFill className={`text-2xl group-hover:text-secondary animation-500`}/>
+                    <span className={`font-medium uppercase ml-4 group-hover:text-secondary animation-500`}>CV Form</span>
                 </Link>
             </nav>
+
+            {/*mobile navbar*/}
+            <div>
+                <div onClick={handleNav} className={`z-[1000] md:hidden fixed bg-secondary text-primary top-[1%] left-[1%] rounded-full p-1 cursor-pointer`}>
+                    <CgArrowsExchange className={`text-xl`}/>
+                </div>
+                <nav className={`z-[1000] md:hidden flex flex-col items-start bg-secondary rounded-xl fixed top-[1%] ${nav?'right-[1%]':'right-[-50%]'} animation-500`}>
+                    <Link href={`/`} className={`w-full group flex items-center justify-start text-primary p-1 cursor-pointer hover:bg-primary animation-500 p-3`}>
+                        <IoHome className={`text-2xl group-hover:text-secondary animation-500`}/>
+                        <span className={`font-medium text-base uppercase group-hover:text-secondary`}>Home</span>
+                    </Link>
+                    <Link href={`/about`} className={`w-full group flex items-center justify-start text-primary p-1 cursor-pointer hover:bg-primary animation-500 p-3`}>
+                        <BsFillInfoCircleFill className={`text-2xl group-hover:text-secondary animation-500`}/>
+                        <span className={`font-medium text-base uppercase group-hover:text-secondary animation-500`}>About</span>
+                    </Link>
+                    <Link href={`/education`} className={`w-full group flex items-center justify-start text-primary p-1 cursor-pointer hover:bg-primary animation-500 p-3`}>
+                        <FaUserGraduate className={`text-2xl group-hover:text-secondary animation-500`}/>
+                        <span className={`font-medium text-base uppercase group-hover:text-secondary animation-500`}>Education</span>
+                    </Link>
+                    <Link href={`/projects`} className={`w-full group flex items-center justify-start text-primary p-1 cursor-pointer hover:bg-primary animation-500 p-3`}>
+                        <FaFolderOpen className={`text-2xl group-hover:text-secondary animation-500`}/>
+                        <span className={`font-medium text-base uppercase group-hover:text-secondary animation-500`}>Projects</span>
+                    </Link>
+                    <Link href={`/cv`} className={`w-full group flex items-center justify-start text-primary p-1 cursor-pointer hover:bg-primary animation-500 p-3`}>
+                        <BsFillPersonVcardFill className={`text-2xl group-hover:text-secondary animation-500`}/>
+                        <span className={`font-medium text-base uppercase group-hover:text-secondary animation-500`}>CV Form</span>
+                    </Link>
+                </nav>
+            </div>
         </>
     );
 };
--- src/pages/cv.js
+++ src/pages/cv.js
@@ -5,6 +5,7 @@ import myCv from "/public/assets/Si_Thu_Kyaw_CV.jpg";
 import Image from "next/image";
 import {BsFiletypePdf} from "react-icons/bs";
 import Link from "next/link";
+
 const Cv = () => {
     return (
         <>
@@ -15,11 +16,24 @@ const Cv = () => {
                 <link rel="icon" href="../../assets/favicon.png"/>
             </Head>
             <Navbar/>
+            <div className={`fixed -z-10 h-full w-full`}>
+                <svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'>
+                    <defs>
+                        <pattern id='a' patternUnits='userSpaceOnUse' width='30' height='30'
+                                 patternTransform='scale(1) rotate(0)'>
+                            <rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 12%, 1)'/>
+                            <path d='M0 22.5h30v15H0zm15-15h30v15H15m-30-15h30v15h-30zm15-15h30v15H0z'
+                                  stroke-width='1.5' stroke='hsla(217, 10%, 25%, 1)' fill='none'/>
+                        </pattern>
+                    </defs>
+                    <rect width='800%' height='800%' transform='translate(-60,-60)' fill='url(#a)'/>
+                </svg>
+            </div>
             <div className={`xl:w-[50%] m-auto p-10 relative xl:block flex justify-center items-center`}>
                 <Image src={myCv} alt={`/`}/>
                 <div className={`absolute md:bottom-[10%] md:left-[10%] bottom-0`}>
                     <Link href="/"
-                       className={`group lg:hover:indent-8 indent-2 btn-black text-white inline-flex items-center justify-center relative animation-500`}>
+                          className={`group lg:hover:indent-8 indent-2 btn-black text-white inline-flex items-center justify-center relative animation-500`}>
                         <BsFiletypePdf
                             className={`group-hover:btn-animation text-2xl h-full animation-500 lg:absolute left-3 top-0 lg:opacity-0`}/>
                         Download CV
